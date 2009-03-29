936bdf15377768f70c35d658a07caa607b33463a
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-03-29 19:29:34 +0000

    new error_view release and log entries
    git-svn-id: http://svn.code.sf.net/p/keepright/code@16 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- web/index.php
+++ web/index.php
@@ -20,27 +20,12 @@ By default you will be put into the center of Vienna.<br>
 Please give a comment that helps me improve the check routines if you find a false-positive. Don't confuse the comment box with an editing feature. This is not potlatch! You cannot add missing tags via keepright!
 
 <h3>logfile</h3>
+<h4>2009-03-29</h4>
+An updated errors-table went online today! Planet dump was updated as of march 25th 2009. Because of my fault this is only a partial update. Checks floating islands, intersections without junctions and overlapping ways are still in the state of last week.<br><br>
+
 <h4>2009-03-21</h4>
 An updated errors-table went online today! Planet dump was updated as of march 17th 2009.<br><br>
 
-<h4>2009-03-14</h4>
-An updated errors-table went online today! Planet dump was updated as of march 10th 2009.<br><br>
-
-<h4>2009-03-10</h4>
-Checks "missing tags" and "highway without ref tag" were redefined and relaxed to better fit real life: "missing tags" won't complain on multipolygon ways tagged as "role inner". "highway without ref tag" does now accept motorways as correctly tagged if any of ref, nat_ref or int_ref tags exist. This change removes 11.686 errors for "missing tags" and 97.054 for "highway without ref tag". Thank you, Ulf, Dermot and Jean-Luc!
-<br><br>
-
-<h4>2009-03-07</h4>
-An updated errors-table went online today! Planet dump was updated as of march 2nd 2009. Due to an error in the scripts many errors were invisible up to now. Thank you, Hans, for the hint! <br><br>
-
-<h4>2009-02-28</h4>
-An updated errors-table went online today! Planet dump was updated as of feb. 22th 2009.<br><br>
-
-<h4>2009-02-21</h4>
-An updated errors-table went online today! Planet dump was updated as of feb. 17th 2009.<br><br>
-Furthermore a completely rewritten user interface is in place now. This work evolved in cooperation with Hans, who provided valuable input and beta testing feedback. Thank you!<br>
-For those who want to find out how much better the new interface is: You can still use the <a href="report_map1.php">old version</a>, but you should not.<br><br>
-You may now mark errors as 'temporarily ignorable' if you just corrected the error and don't want other users to loose time with this issue. These errors will be removed from the map if they are indeed closed, otherwise they will jump back to open state during the next update.<br>If you find a false positive, please mark it as 'permanently ignorable' and please give a short comment that helps me improve my check routines!
 
 <br><br>For archeologists: <a href="logs.php">Old log entries</a> have moved.
 
--- web/logs.php
+++ web/logs.php
@@ -9,6 +9,25 @@ data consistency checks for <a href="http://www.openstreetmap.org">OSM</a><hr>
 
 <h2>historic logs</h2>
 
+<h4>2009-03-14</h4>
+An updated errors-table went online today! Planet dump was updated as of march 10th 2009.<br><br>
+
+<h4>2009-03-10</h4>
+Checks "missing tags" and "highway without ref tag" were redefined and relaxed to better fit real life: "missing tags" won't complain on multipolygon ways tagged as "role inner". "highway without ref tag" does now accept motorways as correctly tagged if any of ref, nat_ref or int_ref tags exist. This change removes 11.686 errors for "missing tags" and 97.054 for "highway without ref tag". Thank you, Ulf, Dermot and Jean-Luc!
+<br><br>
+
+<h4>2009-03-07</h4>
+An updated errors-table went online today! Planet dump was updated as of march 2nd 2009. Due to an error in the scripts many errors were invisible up to now. Thank you, Hans, for the hint! <br><br>
+
+<h4>2009-02-28</h4>
+An updated errors-table went online today! Planet dump was updated as of feb. 22th 2009.<br><br>
+
+<h4>2009-02-21</h4>
+An updated errors-table went online today! Planet dump was updated as of feb. 17th 2009.<br><br>
+Furthermore a completely rewritten user interface is in place now. This work evolved in cooperation with Hans, who provided valuable input and beta testing feedback. Thank you!<br>
+For those who want to find out how much better the new interface is: You can still use the <a href="report_map1.php">old version</a>, but you should not.<br><br>
+You may now mark errors as 'temporarily ignorable' if you just corrected the error and don't want other users to loose time with this issue. These errors will be removed from the map if they are indeed closed, otherwise they will jump back to open state during the next update.<br>If you find a false positive, please mark it as 'permanently ignorable' and please give a short comment that helps me improve my check routines!
+
 <h4>2009-02-04</h4>
 An updated errors-table went online today! Planet dump was updated as of feb. 2nd 2009
 
