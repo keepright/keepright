ddb8d9a76dad3a31a3afc9ba3877f0aabd796659
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-04-06 21:44:42 +0000

    extended ch40 to find nodes that cannot be reached or that cannot be escaped from because of colliding one-ways
    git-svn-id: http://svn.code.sf.net/p/keepright/code@41 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0040_dead-ended_one-ways.php
+++ checks/0040_dead-ended_one-ways.php
@@ -10,82 +10,129 @@ a way tagged as oneway must always be connected to other ways on both end-nodes
 */
 
 
-	query("DROP TABLE IF EXISTS _tmp_one_ways;", $db1);
+query("DROP TABLE IF EXISTS _tmp_one_ways", $db1);
+query("
+	CREATE TABLE _tmp_one_ways (
+	way_id bigint NOT NULL,
+	first_node_id bigint,
+	last_node_id bigint,
+	last_node_lat double precision,
+	last_node_lon double precision,
+	PRIMARY KEY (way_id)
+	)
+", $db1);
 
-	query("
-		CREATE TABLE _tmp_one_ways (
-		way_id bigint NOT NULL,
-		first_node_id bigint,
-		last_node_id bigint,
-		last_node_lat double precision,
-		last_node_lon double precision,
-		PRIMARY KEY (way_id)
-		)
-	", $db1);
+query("
+	INSERT INTO _tmp_one_ways (way_id)
+	SELECT way_id
+	FROM way_tags
+	WHERE (k='oneway' AND v IN ('yes', 'true', '1')) OR
+		(k='highway' AND v IN ('motorway', 'motorway_link'))
+	GROUP BY way_id
+", $db1);
 
-	query("
-		INSERT INTO _tmp_one_ways (way_id)
+query("CREATE INDEX idx_tmp_one_ways_first_node_id ON _tmp_one_ways (first_node_id)", $db1, false);
+query("CREATE INDEX idx_tmp_one_ways_last_node_id ON _tmp_one_ways (last_node_id)", $db1, false);
+
+
+// find id of first and last node as well as coordinates of last node
+query("
+	UPDATE _tmp_one_ways AS c
+	SET first_node_id=w.first_node_id,
+	last_node_id=w.last_node_id,
+	last_node_lat=w.last_node_lat,
+	last_node_lon=w.last_node_lon
+	FROM ways AS w
+	WHERE w.id=c.way_id
+", $db1);
+
+
+// find end nodes that are not connected to any other way
+// exclude ring-ways (firstnode==lastnode)
+query("
+	INSERT INTO _tmp_errors (error_type, object_type, object_id, description, last_checked)
+	SELECT $error_type, 'way', o.way_id, 'The first node (id ' || o.first_node_id || ') of this one-way is not connected to any other way.', NOW()
+	FROM _tmp_one_ways o
+	WHERE o.first_node_id<>o.last_node_id AND
+	NOT EXISTS(
 		SELECT way_id
-		FROM way_tags
-		WHERE k='oneway' AND v IN ('yes', 'true', '1')
-		GROUP BY way_id
-	", $db1);
+		FROM way_nodes wn1
+		WHERE o.first_node_id=wn1.node_id AND wn1.way_id<>o.way_id
+	)
+", $db1);
 
-	query("CREATE INDEX idx_tmp_one_ways_first_node_id ON _tmp_one_ways (first_node_id)", $db1, false);
-	query("CREATE INDEX idx_tmp_one_ways_last_node_id ON _tmp_one_ways (last_node_id)", $db1, false);
+// do the same for the last node of one-ways
+// need to specify lat/lon values here, because if they are left NULL
+// the main script will insert lat/lon of the way's first node
+query("
+	INSERT INTO _tmp_errors (error_type, object_type, object_id, description, last_checked, lat, lon)
+	SELECT $error_type+1, 'way', o.way_id, 'The last node (id ' || o.last_node_id || ') of this one-way is not connected to any other way.', NOW(), 1e7*o.last_node_lat, 1e7*o.last_node_lon
+	FROM _tmp_one_ways o
+	WHERE o.first_node_id<>o.last_node_id AND
+	NOT EXISTS(
+		SELECT way_id
+		FROM way_nodes wn2
+		WHERE o.last_node_id=wn2.node_id AND wn2.way_id<>o.way_id
+	)
+", $db1);
 
 
-	// maybe disable this check on segments of motorways?
-//	query("
-//		DELETE FROM _tmp_one_ways
-//		WHERE EXISTS(
-//			SELECT *
-//			FROM way_tags
-//			WHERE k='highway' AND v='motorway' AND way_id=_tmp_one_ways.way_id
-//		)
-//	", $db1);
+// another point is nodes that are connected to other one-way streets but
+// direction of ways was chosen wrongly so that one-way streets clash:
+//      ------->*<------           <-------*-------->
+// this node is a black hole     this node cannot be reached
 
-	// find id of first and last node as well as coordinates of last node
+// table for one-way streets connected together
+query("DROP TABLE IF EXISTS _tmp_one_way_junctions", $db1, false);
+query("
+	CREATE TABLE _tmp_one_way_junctions (
+	node_id bigint NOT NULL,
+	PRIMARY KEY (node_id)
+	)
+", $db1);
+
+
+// the following has to be done twice: for 'first' and for 'last' nodes
+$cfg = array(
+	'first'=>'This node cannot be reached, because one-ways only lead away from here.',
+	'last'=>'You cannot escape from this node, because one-ways only lead to here.'
+);
+
+$additivum=2;
+foreach ($cfg as $item=>$msg) {
+
+	// find one-way streets connected together by their first (last) node
+	query("TRUNCATE TABLE _tmp_one_way_junctions", $db1, false);
 	query("
-		UPDATE _tmp_one_ways AS c
-		SET first_node_id=w.first_node_id,
-		last_node_id=w.last_node_id,
-		last_node_lat=w.last_node_lat,
-		last_node_lon=w.last_node_lon
-		FROM ways AS w
-		WHERE w.id=c.way_id
-	", $db1);
-
-
-	// find end nodes that are not connected to any other way
-	// exclude ring-ways (firstnode==lastnode)
-	query("
-		INSERT INTO _tmp_errors (error_type, object_type, object_id, description, last_checked) 
-		SELECT $error_type, 'way', o.way_id, 'The first node (id ' || o.first_node_id || ') of this one-way is not connected to any other way.', NOW()
+		INSERT INTO _tmp_one_way_junctions (node_id)
+		SELECT ${item}_node_id
 		FROM _tmp_one_ways o
-		WHERE o.first_node_id<>o.last_node_id AND
-		NOT EXISTS(
+		WHERE o.first_node_id<>o.last_node_id
+		GROUP BY ${item}_node_id
+		HAVING COUNT(way_id)>1
+	", $db1);
+
+	// it is an error, if...
+	// some one-ways are connected by their first (last) nodes,
+	// and there is no other way connected to this junction node
+	// except these one-ways
+	query("
+		INSERT INTO _tmp_errors (error_type, object_type, object_id, description, last_checked)
+		SELECT $error_type+$additivum, 'node', j.node_id, '$msg', NOW()
+		FROM _tmp_one_way_junctions j
+		WHERE NOT EXISTS (
 			SELECT way_id
-			FROM way_nodes wn1
-			WHERE o.first_node_id=wn1.node_id AND wn1.way_id<>o.way_id
+			FROM way_nodes wn
+			WHERE j.node_id=wn.node_id AND wn.way_id NOT IN (
+				SELECT way_id
+				FROM _tmp_one_ways o
+				WHERE o.${item}_node_id=j.node_id
+			)
 		)
 	", $db1);
+	$additivum++;
+}
 
-	// do the same for the last node of one-ways
-	// need to specify lat/lon values here, because if they are left NULL
-	// the main script will insert lat/lon of the way's first node
-	query("
-		INSERT INTO _tmp_errors (error_type, object_type, object_id, description, last_checked, lat, lon) 
-		SELECT $error_type+1, 'way', o.way_id, 'The last node (id ' || o.last_node_id || ') of this one-way is not connected to any other way.', NOW(), 1e7*o.last_node_lat, 1e7*o.last_node_lon
-		FROM _tmp_one_ways o
-		WHERE o.first_node_id<>o.last_node_id AND
-		NOT EXISTS(
-			SELECT way_id
-			FROM way_nodes wn2
-			WHERE o.last_node_id=wn2.node_id AND wn2.way_id<>o.way_id
-		)
-	", $db1);
-
-	query("DROP TABLE IF EXISTS _tmp_one_ways;", $db1);
-
+query("DROP TABLE IF EXISTS _tmp_one_ways", $db1);
+query("TRUNCATE TABLE _tmp_one_way_junctions", $db1);
 ?>
