86115c06aae5c68c9e57cb0e4ce93eaf396a9460
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-04-18 16:08:41 +0000

    exclude areas from overlapping ways check
    git-svn-id: http://svn.code.sf.net/p/keepright/code@59 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0190_intersections_without_junctions.php
+++ checks/0190_intersections_without_junctions.php
@@ -7,133 +7,141 @@
 
 find crossings of highways that are not represented by a common node
 
+to avoid false positives on highways crossing squares, areas are excluded here
+
 */
 
 
-	// tmp_ways will contain all highways with their linestring geometry and layer tag
-	query("DROP TABLE IF EXISTS _tmp_ways", $db1);
+// tmp_ways will contain all highways with their linestring geometry and layer tag
+query("DROP TABLE IF EXISTS _tmp_ways", $db1);
+query("
+	CREATE TABLE _tmp_ways (
+	way_id bigint NOT NULL,
+	layer text
+	)
+", $db1);
+query("SELECT AddGeometryColumn('_tmp_ways', 'geom', 4326, 'LINESTRING', 2)", $db1);
+
+// find any highway-tagged way that is not an area
+query("
+	INSERT INTO _tmp_ways (way_id, geom)
+	SELECT id, geom
+	FROM ways
+	WHERE EXISTS (
+		SELECT wt.v
+		FROM way_tags wt
+		WHERE wt.k='highway' AND wt.way_id=ways.id
+	)
+	AND NOT EXISTS (
+		SELECT wt.v
+		FROM way_tags wt
+		WHERE wt.k='area' AND wt.v='yes' AND wt.way_id=ways.id
+	)
+", $db1);
+
+query("ALTER TABLE _tmp_ways ADD PRIMARY KEY (way_id);", $db1);
+
+// fetch layer tag
+query("
+	UPDATE _tmp_ways c
+	SET layer=t.v
+	FROM way_tags t
+	WHERE t.way_id=c.way_id AND t.k='layer'
+", $db1);
+
+// set default layers:
+// bridges have layer +1 (if no layer tag is given)
+// tunnels have layer -1 (if no layer tag is given)
+// anything else has layer 0 (if no layer tag is given)
+query("
+	UPDATE _tmp_ways c
+	SET layer=1
+	FROM way_tags t
+	WHERE layer IS NULL AND
+	t.way_id=c.way_id AND
+	t.k='bridge'
+", $db1);
+query("
+	UPDATE _tmp_ways c
+	SET layer=-1
+	FROM way_tags t
+	WHERE layer IS NULL AND
+	t.way_id=c.way_id AND
+	t.k='tunnel'
+", $db1);
+query("
+	UPDATE _tmp_ways c
+	SET layer=0
+	WHERE layer IS NULL
+", $db1);
+
+query("CREATE INDEX idx_tmp_ways_layer ON _tmp_ways (layer)", $db1);
+query("CREATE INDEX idx_tmp_ways_geom ON _tmp_ways USING gist (geom)", $db1);
+
+
+// find ways that graphically intersect (i.e. cross or overlap)
+// intersecting is not an error if ways share a common node; this will be checked later
+$result=query("
+	SELECT w1.way_id as way_id1, w2.way_id as way_id2, asText(ST_intersection(w1.geom, w2.geom)) AS geom
+	FROM _tmp_ways w1, _tmp_ways w2
+	WHERE w1.layer=w2.layer AND
+		w1.way_id<w2.way_id AND
+		ST_crosses(w1.geom, w2.geom)
+", $db1);
+
+// ST_intersection() may return one of the following geometry types:
+// POINT() for one single intersection point
+// MULTIPOINT() if ways intersect in more than one spot
+// LINESTRING() if ways overlap (ie. segments running on the same line)
+// MULTILINESTRING() if more than one overlapping occurs
+// GEOMETRYCOLLECTION() if a combination of the above is found
+// This geometry is a container for different sub-geometries of above types.
+
+while ($row=pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
+
+	$points = get_startingpoints($row['geom']);
+
+	foreach ($points as $point)
+		if (!connected_near($row['way_id1'], $row['way_id2'], $point[0], $point[1], $db2)) {
+			query("
+				INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked, lon, lat)
+				VALUES($error_type, CAST('way' AS type_object_type), {$row['way_id1']},
+				'This way intersects way #' || {$row['way_id2']} || ' but there is no junction node', NOW()," . 
+				round(1e7*merc_lon($point[0])) . ',' . round(1e7*merc_lat($point[1])) . ')'
+			, $db2, false);
+
+		}
+
+}
+pg_free_result($result);
+
+
+
+// now look for overlapping ways
+// that is ways that (partly) use the same sequences of nodes.
+// Such segments lie on top of each other and are not covered 
+// by the intersections-test above
+$result=query("
+	SELECT w1.way_id as way_id1, w2.way_id as way_id2, asText(ST_intersection(w1.geom, w2.geom)) AS geom
+	FROM _tmp_ways w1, _tmp_ways w2
+	WHERE w1.layer=w2.layer AND
+		w1.way_id<w2.way_id AND
+		ST_overlaps(w1.geom, w2.geom)
+", $db1);
+
+while ($row=pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
+
+	$points = get_startingpoints($row['geom']);
+	$point = $points[0];
 	query("
-		CREATE TABLE _tmp_ways (
-		way_id bigint NOT NULL,
-		layer text
-		)
-	", $db1);
-	query("SELECT AddGeometryColumn('_tmp_ways', 'geom', 4326, 'LINESTRING', 2)", $db1);
+		INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked, lon, lat) 
+		VALUES($error_type+10, CAST('way' AS type_object_type), {$row['way_id1']},
+		'This way overlaps way #' || {$row['way_id2']} || '.', NOW()," .
+		1e7*merc_lon($point[0]) . ',' . 1e7*merc_lat($point[1]) . ')'
+	, $db2, false);
 
-	// find any highway-tagged way
-	query("
-		INSERT INTO _tmp_ways (way_id, geom)
-		SELECT id, geom
-		FROM ways
-		WHERE EXISTS (
-			SELECT wt.v
-			FROM way_tags wt
-			WHERE wt.k='highway' AND wt.way_id=ways.id
-		)
-	", $db1);
-	query("ALTER TABLE _tmp_ways ADD PRIMARY KEY (way_id);", $db1);
-
-	// fetch layer tag
-	query("
-		UPDATE _tmp_ways c
-		SET layer=t.v
-		FROM way_tags t
-		WHERE t.way_id=c.way_id AND t.k='layer'
-	", $db1);
-
-	// set default layers:
-	// bridges have layer +1 (if no layer tag is given)
-	// tunnels have layer -1 (if no layer tag is given)
-	// anything else has layer 0 (if no layer tag is given)
-	query("
-		UPDATE _tmp_ways c
-		SET layer=1
-		FROM way_tags t
-		WHERE layer IS NULL AND
-		t.way_id=c.way_id AND
-		t.k='bridge'
-	", $db1);
-	query("
-		UPDATE _tmp_ways c
-		SET layer=-1
-		FROM way_tags t
-		WHERE layer IS NULL AND
-		t.way_id=c.way_id AND
-		t.k='tunnel'
-	", $db1);
-	query("
-		UPDATE _tmp_ways c
-		SET layer=0
-		WHERE layer IS NULL
-	", $db1);
-
-	query("CREATE INDEX idx_tmp_ways_layer ON _tmp_ways (layer)", $db1);
-	query("CREATE INDEX idx_tmp_ways_geom ON _tmp_ways USING gist (geom)", $db1);
-
-
-	// find ways that graphically intersect (i.e. cross or overlap)
-	// intersecting is not an error if ways share a common node; this will be checked later
-	$result=query("
-		SELECT w1.way_id as way_id1, w2.way_id as way_id2, asText(ST_intersection(w1.geom, w2.geom)) AS geom
-		FROM _tmp_ways w1, _tmp_ways w2
-		WHERE w1.layer=w2.layer AND
-			w1.way_id<w2.way_id AND
-			ST_crosses(w1.geom, w2.geom)
-	", $db1);
-
-	// ST_intersection() may return one of the following geometry types:
-	// POINT() for one single intersection point
-	// MULTIPOINT() if ways intersect in more than one spot
-	// LINESTRING() if ways overlap (ie. segments running on the same line)
-	// MULTILINESTRING() if more than one overlapping occurs
-	// GEOMETRYCOLLECTION() if a combination of the above is found
-	// This geometry is a container for different sub-geometries of above types.
-
-	while ($row=pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
-
-		$points = get_startingpoints($row['geom']);
-
-		foreach ($points as $point)
-			if (!connected_near($row['way_id1'], $row['way_id2'], $point[0], $point[1], $db2)) {
-				query("
-					INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked, lon, lat)
-					VALUES($error_type, CAST('way' AS type_object_type), {$row['way_id1']},
-					'This way intersects way #' || {$row['way_id2']} || ' but there is no junction node', NOW()," . 
-					1e7*merc_lon($point[0]) . ',' . 1e7*merc_lat($point[1]) . ')'
-				, $db2, false);
-
-			}
-
-	}
-	pg_free_result($result);
-
-
-
-	// now look for overlapping ways
-	// that is ways that (partly) use the same sequences of nodes.
-	// Such segments lie on top of each other and are not covered 
-	// by the intersections-test above
-	$result=query("
-		SELECT w1.way_id as way_id1, w2.way_id as way_id2, asText(ST_intersection(w1.geom, w2.geom)) AS geom
-		FROM _tmp_ways w1, _tmp_ways w2
-		WHERE w1.layer=w2.layer AND
-			w1.way_id<w2.way_id AND
-			ST_overlaps(w1.geom, w2.geom)
-	", $db1);
-
-	while ($row=pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
-
-		$points = get_startingpoints($row['geom']);
-		$point = $points[0];
-		query("
-			INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked, lon, lat) 
-			VALUES($error_type+10, CAST('way' AS type_object_type), {$row['way_id1']},
-			'This way overlaps way #' || {$row['way_id2']} || '.', NOW()," .
-			1e7*merc_lon($point[0]) . ',' . 1e7*merc_lat($point[1]) . ')'
-		, $db2, false);
-
-	}
-	pg_free_result($result);
+}
+pg_free_result($result);
 
 
 query("DROP TABLE IF EXISTS _tmp_ways", $db1, false);
