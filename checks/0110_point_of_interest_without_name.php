138cd8c8e76588bbfdf26282d75edbfaf1933733
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-04-19 19:41:14 +0000

    require an operator tag instead of a name tag for banks
    git-svn-id: http://svn.code.sf.net/p/keepright/code@64 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0110_point_of_interest_without_name.php
+++ checks/0110_point_of_interest_without_name.php
@@ -2,7 +2,7 @@
 
 
 /*
-inspired by maplint points of interest are reported if they lack a name tag
+inspired by maplint points of interest are reported if they lack a name (or operator) tag
 */
 
 
@@ -11,10 +11,9 @@ query("
 	CREATE OR REPLACE VIEW _tmp_points_of_interest AS
 	SELECT node_id, MIN(v) as v
 	FROM node_tags
-	WHERE k='amenity' AND v IN ('place_of_worship', 'cinema', 'pharmacy', 'cafe', 'fast_food', 'pub', 'restaurant', 'school', 'university', 'hospital', 'library', 'theatre', 'courthouse', 'bank')
+	WHERE k='amenity' AND v IN ('place_of_worship', 'cinema', 'pharmacy', 'cafe', 'fast_food', 'pub', 'restaurant', 'school', 'university', 'hospital', 'library', 'theatre', 'courthouse')
 	GROUP BY node_id
 ", $db1);
-
 query("
 	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
 	SELECT $error_type, 'node', node_id, 'This node is tagged as ' || v || ' and therefore needs a name tag', NOW()
@@ -22,6 +21,21 @@ query("
 	WHERE NOT EXISTS( SELECT * FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='name' )
 ", $db1);
 
-query("DROP VIEW _tmp_points_of_interest", $db1);
 
+query("
+	CREATE OR REPLACE VIEW _tmp_points_of_interest AS
+	SELECT node_id, MIN(v) as v
+	FROM node_tags
+	WHERE k='amenity' AND v IN ('bank')
+	GROUP BY node_id
+", $db1);
+query("
+	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
+	SELECT $error_type, 'node', node_id, 'This node is tagged as ' || v || ' and therefore needs an operator tag', NOW()
+	FROM _tmp_points_of_interest b
+	WHERE NOT EXISTS( SELECT * FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='operator' )
+", $db1);
+
+
+query("DROP VIEW _tmp_points_of_interest", $db1);
 ?>
