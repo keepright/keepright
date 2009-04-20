3584892bb95944328549df80291729792637f29c
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-04-20 18:51:03 +0000

    minor change
    git-svn-id: http://svn.code.sf.net/p/keepright/code@66 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/run-checks.php
+++ checks/run-checks.php
@@ -345,10 +345,10 @@ order by last_checked>'2009-03-09', state, error_type
 /*
 UPDATE comments_osm_EU inner join error_view_osm_EU using (error_id) 
 SET comments_osm_EU.state=null, 
-comments_osm_EU.comment=CONCAT("[error still open, 2009-04-14]", comments_osm_EU.comment)
+comments_osm_EU.comment=CONCAT("[error still open, 2009-04-14] ", comments_osm_EU.comment)
 WHERE comments_osm_EU.state='ignore_temporarily' AND 
 error_view_osm_EU.state<>'cleared' AND 
-comments_osm_EU.timestamp<"2009-04-01"
+comments_osm_EU.timestamp<"2009-04-14"
 */
 
 
