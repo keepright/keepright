d7e96b2a5c262081eaa996c1a65508d52eb56397
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-05-10 17:02:01 +0000

    scheduled database update
    git-svn-id: http://svn.code.sf.net/p/keepright/code@79 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- web/index.php
+++ web/index.php
@@ -23,6 +23,9 @@ By default you will be put into the center of Vienna for the European version.<b
 Please give a comment that helps me improve the check routines if you find a false-positive. Don't confuse the comment box with an editing feature. This is not potlatch! You cannot add missing tags via keepright!
 
 <h3>logfile</h3>
+<h4>2009-05-09</h4>
+An updated errors-table went online today! Planet dump was updated as of may 5th 2009.<br><br>
+
 <h4>2009-05-02</h4>
 An updated errors-table went online today! Planet dump was updated as of april 28th 2009.<br><br>
 Waterways are included by now in the checks 'intersections without junctions' and 'overlapping ways'. This will identify spots where bridges are missing.
