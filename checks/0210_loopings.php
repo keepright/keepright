8b4255487586d7a04a6a75a71a4fcb62aeaddbf6
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-07-17 18:37:36 +0000

    fix 210 fails with two nodes in the same spot being used more than once
    git-svn-id: http://svn.code.sf.net/p/keepright/code@126 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0210_loopings.php
+++ checks/0210_loopings.php
@@ -9,9 +9,9 @@ there should be two types of ways:
 
 anything else like A,B,C,D,A,B looks like an error.
 
-So any way may contain up to one node twice at most. 
-If any node is found more than twice in the same way or 
-if more than one node is found twice it looks like an error. 
+So any way may contain up to one node twice at most.
+If any node is found more than twice in the same way or
+if more than one node is found twice it looks like an error.
 */
 
 
@@ -25,7 +25,7 @@ query("
 	)
 ", $db1);
 
-// select all ways that contain nodes at least twice  
+// select all ways that contain nodes at least twice
 query("
 	INSERT INTO _tmp_node_count
 	SELECT way_id, node_id, COUNT(sequence_id) as node_count
@@ -35,27 +35,52 @@ query("
 ", $db1);
 
 
+
 // first part:
 // any way that contains any single node more than twice is considered an error
+
+query("DROP TABLE IF EXISTS _tmp_tmp_errors", $db1, false);
 query("
-	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked, lat, lon) 
-	SELECT $error_type, 'way', c.way_id, 'This way contains node #' || c.node_id || ' ' || c.node_count || ' times. This may or may not be an error.'
-		, NOW(), 1e7*n.lat, 1e7*n.lon
+	SELECT c.way_id, c.node_id, c.node_count, 1e7*n.lat as lat, 1e7*n.lon as lon
+	INTO _tmp_tmp_errors
 	FROM _tmp_node_count c INNER JOIN nodes n ON (c.node_id=n.id)
 	WHERE c.node_count>2
 ", $db1);
+
+// there have been cases when a way contained two nodes more than twice,
+// that where located in exactly the same spot.
+// to ensure uniqueness of way_id/lat/lon we have to decide which node to report
+// let's take the node with biggest node_count. Remember: there could
+// be two node ids with the same number of occurrences, so just pick the larger id one...
+query("
+	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked, lat, lon)
+	SELECT $error_type, 'way', c.way_id, 'This way contains node #' || c.node_id || ' ' || c.node_count || ' times. This may or may not be an error.', NOW(), c.lat, c.lon
+	FROM _tmp_tmp_errors c
+	WHERE node_id=(
+		SELECT MAX(node_id)
+		FROM _tmp_tmp_errors tmp
+		WHERE tmp.way_id=c.way_id and tmp.lat=c.lat and tmp.lon=c.lon
+		AND node_count=(
+			SELECT MAX(node_count)
+			FROM _tmp_tmp_errors tmp2
+			WHERE  tmp2.way_id=c.way_id and tmp2.lat=c.lat and tmp2.lon=c.lon
+		)
+	)
+", $db1);
+query("DROP TABLE IF EXISTS _tmp_tmp_errors", $db1, false);
+
 
 
 // second part:
 // any way may contain just one node twice. If more than one node is found twice it is considered an error
 query("
-        INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
+        INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked)
 	SELECT 1+$error_type, 'way', c.way_id, 'This way contains more than one node at least twice. Nodes are ' || array_to_string(array(
 
 		SELECT '#' || t.node_id
 		FROM _tmp_node_count t
-		WHERE t.way_id=c.way_id	
-	
+		WHERE t.way_id=c.way_id
+
 	), ', ') || '. This may or may not be an error.', NOW()
 	FROM _tmp_node_count c
 	GROUP BY c.way_id
