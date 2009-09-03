6afaaa6f1ffe2c0cf44b6c8bd7760d5d7b6a9cd9
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-09-03 20:12:55 +0000

    add more oneway-type highways, consider reverse-tag, cosider oneway=no tag
    git-svn-id: http://svn.code.sf.net/p/keepright/code@180 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0040_dead-ended_one-ways.php
+++ checks/0040_dead-ended_one-ways.php
@@ -14,8 +14,11 @@ query("DROP TABLE IF EXISTS _tmp_one_ways", $db1);
 query("
 	CREATE TABLE _tmp_one_ways (
 	way_id bigint NOT NULL,
+	reversed boolean DEFAULT false,
 	first_node_id bigint,
 	last_node_id bigint,
+	first_node_lat double precision,
+	first_node_lon double precision,
 	last_node_lat double precision,
 	last_node_lon double precision,
 	PRIMARY KEY (way_id)
@@ -24,45 +27,80 @@ query("
 
 
 // fetch all one-way tagged ways
-// that are ways with oneway=yes/true/1
+// that are ways with oneway=yes/true/1/reverse/-1
 // and all motorways (tagged implicitly)
-// and all motorway_links that don't have a oneway=no/false/0 tag
+// and all *_links that don't have a oneway=no/false/0 tag
 query("
 	INSERT INTO _tmp_one_ways (way_id)
 	SELECT way_id
 	FROM way_tags
-	WHERE (k='oneway' AND v IN ('yes', 'true', '1')) OR
-		(k='highway' AND v = 'motorway') OR
-		(k='highway' AND v = 'motorway_link' AND NOT EXISTS(
-			SELECT tmp.k
-			FROM way_tags tmp
-			WHERE tmp.way_id=way_tags.way_id AND
-			tmp.k='oneway' AND tmp.v IN ('no', 'false', '0')
-		))
+	WHERE (k='oneway' AND v IN ('yes', 'true', '1', 'reverse', '-1')) OR
+		(k='junction' AND v = 'roundabout') OR
+		(k='highway' AND v IN ('motorway', 'motorway_link', 'trunk_link',
+		'primary_link', 'secondary_link'))
 	GROUP BY way_id
 ", $db1);
 
+
 query("CREATE INDEX idx_tmp_one_ways_first_node_id ON _tmp_one_ways (first_node_id)", $db1, false);
 query("CREATE INDEX idx_tmp_one_ways_last_node_id ON _tmp_one_ways (last_node_id)", $db1, false);
+query("CREATE INDEX idx_tmp_one_ways_way_id ON _tmp_one_ways (way_id)", $db1, false);
+
+// implicitly oneway-tagged ways may be tagged non-oneway here
+// mustly applicable to motorway_link, trunk_link, primary_link, secondary_link
+query("
+	DELETE FROM _tmp_one_ways
+	WHERE way_id IN (
+		SELECT way_id
+		FROM way_tags tmp
+		WHERE tmp.k='oneway' AND tmp.v IN ('no', 'false', '0')
+	)
+", $db1);
 
 
-// find id of first and last node as well as coordinates of last node
+query("
+	UPDATE _tmp_one_ways AS c
+	SET reversed=true
+	FROM way_tags tmp
+	WHERE tmp.way_id=c.way_id AND
+	tmp.k='oneway' AND tmp.v IN ('reverse', '-1')
+", $db1);
+
+// find id of first and last node as well as coordinates of first and last node
 query("
 	UPDATE _tmp_one_ways AS c
 	SET first_node_id=w.first_node_id,
+	first_node_lat=w.first_node_lat,
+	first_node_lon=w.first_node_lon,
 	last_node_id=w.last_node_id,
 	last_node_lat=w.last_node_lat,
 	last_node_lon=w.last_node_lon
 	FROM ways AS w
-	WHERE w.id=c.way_id
+	WHERE NOT c.reversed AND w.id=c.way_id
+", $db1);
+
+// oneways tagged with oneway=reverse or oneway=-1 are oneways
+// running in opposite direction (for some reason it isn't
+// possible to change orientation of the way so this tag was chosen)
+// assign first and last nodes the other way round:
+query("
+	UPDATE _tmp_one_ways AS c
+	SET first_node_id=w.last_node_id,
+	last_node_id=w.first_node_id,
+	first_node_lat=w.last_node_lat,
+	first_node_lon=w.last_node_lon,
+	last_node_lat=w.first_node_lat,
+	last_node_lon=w.first_node_lon
+	FROM ways AS w
+	WHERE c.reversed AND w.id=c.way_id
 ", $db1);
 
 
 // find end nodes that are not connected to any other way
 // exclude ring-ways (firstnode==lastnode)
 query("
-	INSERT INTO _tmp_errors (error_type, object_type, object_id, description, last_checked)
-	SELECT $error_type, 'way', o.way_id, 'The first node (id ' || o.first_node_id || ') of this one-way is not connected to any other way.', NOW()
+	INSERT INTO _tmp_errors (error_type, object_type, object_id, description, last_checked, lat, lon)
+	SELECT $error_type, 'way', o.way_id, 'The first node (id ' || o.first_node_id || ') of this one-way is not connected to any other way.', NOW(), 1e7*o.first_node_lat, 1e7*o.first_node_lon
 	FROM _tmp_one_ways o
 	WHERE o.first_node_id<>o.last_node_id AND
 	NOT EXISTS(
@@ -73,8 +111,6 @@ query("
 ", $db1);
 
 // do the same for the last node of one-ways
-// need to specify lat/lon values here, because if they are left NULL
-// the main script will insert lat/lon of the way's first node
 query("
 	INSERT INTO _tmp_errors (error_type, object_type, object_id, description, last_checked, lat, lon)
 	SELECT $error_type+1, 'way', o.way_id, 'The last node (id ' || o.last_node_id || ') of this one-way is not connected to any other way.', NOW(), 1e7*o.last_node_lat, 1e7*o.last_node_lon
