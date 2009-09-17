7ee602d69647f48cf306d32e12644d8514bb04c6
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-09-17 20:46:31 +0000

    new make job for Canada and Central America
    git-svn-id: http://svn.code.sf.net/p/keepright/code@195 4e67cf90-0d29-4caa-83dc-599f2b650fb7

---
+++ checks/make.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# script for updating multiple keepright databases
+#
+
+#./updateDB.sh AT XT >> make.log 2>&1
+
+
+for i do	# loop all given parameter values
+
+	case "$i" in
+
+		EU)
+			./updateDB.sh AT CZ DE DK FI PL SE XH XJ XK XL XM XN XO XP XQ XR XS XT >> make_EU.log 2>&1
+			php export_errors.php EU >> make_EU.log 2>&1
+			./updateWebDB.sh EU >> make_EU.log 2>&1
+		;;
+
+		US)
+			./updateDB.sh USAL USAK USAZ USAR USCA USCO USFL USGA USGU USHI USID USIL USIN USIA USKS USKY USLA USMA USMI USMN USMS USMO USMT USNE USNV USNJ USNM USNY USNC USND USOH USOK USOR USPA USSC USSD USTN USTX USUT USVA USWA USWV USWI USWY XE XF >> make_US.log 2>&1
+			php export_errors.php US >> make_US.log 2>&1
+			./updateWebDB.sh US >> make_US.log 2>&1
+		;;
+
+                CA)
+               		./updateDB.sh CA XG >> make.log 2>&1
+			php export_errors.php CA XG >> make.log 2>&1
+                        ./updateWebDB.sh CA XG >> make.log 2>&1
+                ;;
+	esac
+done
