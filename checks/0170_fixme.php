a964e4476f4b746d0bb40bd7c7822bc40c9a1116
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-10-26 20:06:59 +0000

    make fixme check case insensitive
    git-svn-id: http://svn.code.sf.net/p/keepright/code@234 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0170_fixme.php
+++ checks/0170_fixme.php
@@ -7,6 +7,7 @@ find any way/node/relation that is tagged with FIXME anywhere in key or value
 
 $tables=array('node', 'way', 'relation');
 // execute similar queries for all three *_tags tables
+// according to the Wiki 'fixme' is valid as well as 'FIXME' so we search case sensitive here
 foreach($tables as $table) {
 
 	query("
@@ -14,11 +15,11 @@ foreach($tables as $table) {
 		SELECT $error_type, '$table', {$table}_id, 'This {$table} is fixme-tagged: ' || array_to_string(array(
 			SELECT '\"' || COALESCE(k,'') || '=' || COALESCE(v,'') || '\"'
 			FROM {$table}_tags AS tmp
-			WHERE tmp.{$table}_id=t.{$table}_id AND (tmp.k LIKE '%fixme%' OR tmp.v LIKE '%fixme%' OR (k='name' AND v='tbd') OR (k='ref' AND v='tbd'))
+			WHERE tmp.{$table}_id=t.{$table}_id AND (tmp.k iLIKE '%fixme%' OR tmp.v iLIKE '%fixme%' OR (k='name' AND v='tbd') OR (k='ref' AND v='tbd'))
 		), ' and '), NOW()
 
 		FROM {$table}_tags t
-		WHERE (k LIKE '%fixme%' OR v LIKE '%fixme%' OR (k='name' AND v='tbd') OR (k='ref' AND v='tbd'))
+		WHERE (k iLIKE '%fixme%' OR v iLIKE '%fixme%' OR (k='name' AND v='tbd') OR (k='ref' AND v='tbd'))
 		GROUP BY {$table}_id
 	", $db1);
 
