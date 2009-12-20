2c8aa6b3aa29f7bba427dfa8b1e1abe8e68099b2
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-12-20 21:58:39 +0000

    change keepright to cut planet file on your own
    git-svn-id: http://svn.code.sf.net/p/keepright/code@256 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- planet/pgsql_simple_load.sql
+++ planet/pgsql_simple_load.sql
@@ -11,32 +11,7 @@ AXIS["y", NORTH],AUTHORITY["EPSG","900913"]]',
 '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs');
 
 
--- Drop all primary keys and indexes to improve load speed.
-ALTER TABLE nodes DROP CONSTRAINT pk_nodes;
-ALTER TABLE ways DROP CONSTRAINT pk_ways;
-ALTER TABLE way_nodes DROP CONSTRAINT pk_way_nodes;
-ALTER TABLE relations DROP CONSTRAINT pk_relations;
 
-DROP INDEX IF EXISTS idx_node_tags_node_id;
-DROP INDEX IF EXISTS idx_nodes_geom;
-DROP INDEX IF EXISTS idx_way_tags_way_id;
-DROP INDEX IF EXISTS idx_way_nodes_node_id;
-DROP INDEX IF EXISTS idx_relation_tags_relation_id;
-DROP INDEX IF EXISTS idx_ways_bbox;
-
-DROP INDEX IF EXISTS idx_relations_member_id;
-DROP INDEX IF EXISTS idx_relations_member_role;
-DROP INDEX IF EXISTS idx_relations_member_type;
-
-DROP INDEX IF EXISTS idx_node_tags_k;
-DROP INDEX IF EXISTS idx_node_tags_v;
-DROP INDEX IF EXISTS idx_way_tags_k;
-DROP INDEX IF EXISTS idx_way_tags_v;
-DROP INDEX IF EXISTS idx_relation_tags_k;
-DROP INDEX IF EXISTS idx_relation_tags_v;
-
-SELECT DropGeometryColumn('ways', 'bbox');
-SELECT DropGeometryColumn('ways', 'geom');
 
 -- Import the table data from the data files using the fast COPY method.
 \copy nodes FROM 'pgimport/nodes_sorted.txt' WITH NULL AS 'NULL'
@@ -49,15 +24,34 @@ SELECT DropGeometryColumn('ways', 'geom');
 \copy relation_members FROM 'pgimport/relation_members.txt' WITH NULL AS 'NULL'
 
 -- Add the primary keys and indexes back again (except the way bbox index).
+ALTER TABLE ONLY schema_info ADD CONSTRAINT pk_schema_info PRIMARY KEY (version);
 ALTER TABLE ONLY nodes ADD CONSTRAINT pk_nodes PRIMARY KEY (id);
 ALTER TABLE ONLY ways ADD CONSTRAINT pk_ways PRIMARY KEY (id);
 ALTER TABLE ONLY way_nodes ADD CONSTRAINT pk_way_nodes PRIMARY KEY (way_id, sequence_id);
 ALTER TABLE ONLY relations ADD CONSTRAINT pk_relations PRIMARY KEY (id);
+
 CREATE INDEX idx_node_tags_node_id ON node_tags USING btree (node_id);
 CREATE INDEX idx_nodes_geom ON nodes USING gist (geom);
+
 CREATE INDEX idx_way_tags_way_id ON way_tags USING btree (way_id);
 CREATE INDEX idx_way_nodes_node_id ON way_nodes USING btree (node_id);
+
 CREATE INDEX idx_relation_tags_relation_id ON relation_tags USING btree (relation_id);
 CREATE INDEX idx_relations_member_id ON relation_members USING btree (member_id);
 CREATE INDEX idx_relations_member_role ON relation_members USING btree (member_role);
 CREATE INDEX idx_relations_member_type ON relation_members USING btree (member_type);
+
+
+-- Index for keys and values
+DELETE FROM way_tags WHERE v IS NULL;
+DELETE FROM node_tags WHERE v IS NULL;
+
+CREATE INDEX idx_node_tags_k ON node_tags (k);
+CREATE INDEX idx_node_tags_v ON node_tags (v);
+
+CREATE INDEX idx_way_tags_k ON way_tags (k);
+CREATE INDEX idx_way_tags_v ON way_tags (v);
+
+CREATE INDEX idx_relation_tags_k ON relation_tags (k);
+CREATE INDEX idx_relation_tags_v ON relation_tags (v);
+
--- planet/pgsql_simple_schema.sql
+++ planet/pgsql_simple_schema.sql
@@ -1,56 +1,66 @@
 -- Database creation script for the simple PostgreSQL schema.
-
--- Drop all tables if they exist.
-DROP TABLE IF EXISTS nodes;
-DROP TABLE IF EXISTS node_tags;
-DROP TABLE IF EXISTS ways;
-DROP TABLE IF EXISTS way_nodes;
-DROP TABLE IF EXISTS way_tags;
-DROP TABLE IF EXISTS relations;
-DROP TABLE IF EXISTS relation_members;
-DROP TABLE IF EXISTS relation_tags;
-DROP TABLE IF EXISTS schema_info;
+-- modified to include more redundancy by Harald Kleiner
 
 
 -- Create a table which will contain a single row defining the current schema version.
 CREATE TABLE schema_info (
     version integer NOT NULL
 );
-
+INSERT INTO schema_info VALUES (1);
 
 -- Create a table for nodes.
 CREATE TABLE nodes (
     id bigint NOT NULL,
-    user_name text NOT NULL,
+    user_name text,
     tstamp timestamp without time zone NOT NULL
 );
 -- Add a postgis point column holding the location of the node.
 SELECT AddGeometryColumn('nodes', 'geom', 4326, 'POINT', 2);
 
+ALTER TABLE nodes
+    ADD lat double precision,
+    ADD lon double precision,
+    ADD x double precision,
+    ADD y double precision;
+
+
+
 -- Create a table for node tags.
 CREATE TABLE node_tags (
     node_id bigint NOT NULL,
     k text NOT NULL,
-    v text NOT NULL
+    v text
 );
 
 
 -- Create a table for ways.
 CREATE TABLE ways (
     id bigint NOT NULL,
-    user_name text NOT NULL,
-    tstamp timestamp without time zone NOT NULL
+    user_name text,
+    tstamp timestamp without time zone NOT NULL,
+    first_node_id bigint,
+    last_node_id bigint,
+    first_node_lat double precision,
+    first_node_lon double precision,
+    first_node_x double precision,
+    first_node_y double precision,
+    last_node_lat double precision,
+    last_node_lon double precision,
+    last_node_x double precision,
+    last_node_y double precision,
+    node_count integer
 );
--- Add a postgis bounding box column used for indexing the location of the way.
--- This will contain a bounding box surrounding the extremities of the way.
-SELECT AddGeometryColumn('ways', 'bbox', 4326, 'GEOMETRY', 2);
 
 
 -- Create a table for representing way to node relationships.
 CREATE TABLE way_nodes (
     way_id bigint NOT NULL,
     node_id bigint NOT NULL,
-    sequence_id smallint NOT NULL
+    sequence_id integer NOT NULL,
+    lat double precision,
+    lon double precision,
+    x double precision,
+    y double precision
 );
 
 
@@ -65,7 +75,7 @@ CREATE TABLE way_tags (
 -- Create a table for relations.
 CREATE TABLE relations (
     id bigint NOT NULL,
-    user_name text NOT NULL,
+    user_name text,
     tstamp timestamp without time zone NOT NULL
 );
 
@@ -82,40 +92,6 @@ CREATE TABLE relation_members (
 CREATE TABLE relation_tags (
     relation_id bigint NOT NULL,
     k text NOT NULL,
-    v text NOT NULL
+    v text
 );
 
-
--- Configure the schema version.
-INSERT INTO schema_info (version) VALUES (1);
-
-
--- Add primary keys to tables.
-
-ALTER TABLE ONLY schema_info ADD CONSTRAINT pk_schema_info PRIMARY KEY (version);
-
-
-ALTER TABLE ONLY nodes ADD CONSTRAINT pk_nodes PRIMARY KEY (id);
-
-
-ALTER TABLE ONLY ways ADD CONSTRAINT pk_ways PRIMARY KEY (id);
-
-
-ALTER TABLE ONLY way_nodes ADD CONSTRAINT pk_way_nodes PRIMARY KEY (way_id, sequence_id);
-
-
-ALTER TABLE ONLY relations ADD CONSTRAINT pk_relations PRIMARY KEY (id);
-
-
--- Add indexes to tables.
-
-CREATE INDEX idx_node_tags_node_id ON node_tags USING btree (node_id);
-CREATE INDEX idx_nodes_geom ON nodes USING gist (geom);
-
-
-CREATE INDEX idx_way_tags_way_id ON way_tags USING btree (way_id);
-CREATE INDEX idx_ways_bbox ON ways USING gist (bbox);
-CREATE INDEX idx_way_nodes_node_id ON way_nodes USING btree (node_id);
-
-
-CREATE INDEX idx_relation_tags_relation_id ON relation_tags USING btree (relation_id);
