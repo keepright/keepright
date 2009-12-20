00ba61aa10064d3de789dc11d6c2c320574f2c97
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-12-20 21:10:28 +0000

    change keepright to cut planet file on your own
    git-svn-id: http://svn.code.sf.net/p/keepright/code@254 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/export_errors.php
+++ checks/export_errors.php
@@ -4,40 +4,40 @@
 export errors
 */
 
-if ($argc<2) {
-	echo "Usage: \">php export_errors.php AT\"";
+if ($argc<3 || ($argv[1]<>'--db' && $argv[1]<>'--schema')) {
+	echo "Usage: \">php export_errors.php --db EU | --schema AT\"";
 	echo "will export entries from error_view into a text dump file\n";
 	echo "database credentials are configured in config";
 	exit;
 }
 
+if ($argv[1]=='--db') {
+	$MAIN_DB_NAME='osm_' . $argv[2];
+	$dbschema='public';
+} else {
+	$schema=$argv[2];
+	$dbschema='schema' . $argv[2];
+}
+
 require('config.inc.php');
 require('helpers.inc.php');
-require('BufferedInserter.php');
 
-
-echo "exporting errors for $db_postfix into dumpfile\n";
+echo "exporting errors for $MAIN_DB_NAME.$dbschema into dumpfile\n";
 
 $db1 = pg_pconnect($connectstring, PGSQL_CONNECT_FORCE_NEW);
 
-$schema=$db_params[$db_postfix]['MAIN_SCHEMA_NAME'];
 
-$fname=$ERROR_VIEW_FILE .'_'. ((strlen($schema)>0 && $schema!='public') ? $schema : $MAIN_DB_NAME) . '.txt';
+$fname=$ERROR_VIEW_FILE .'_'. (isset($schema) ? $schema : $MAIN_DB_NAME) . '.txt';
 $f = fopen($fname, 'w');
-echo "schema: $schema\n";
-
-if (strlen($schema)>0 && $schema!='public')
-	$schemaselector=" AND schema='$schema'";
-else
-	$schemaselector='';
 
 
 if ($f) {
 
 	$result = query("
 		SELECT *, date_trunc('hour',first_occurrence) AS fo, date_trunc('hour',last_checked) AS lc, date_trunc('second',object_timestamp) AS ts
-		FROM error_view
-		WHERE description NOT LIKE '%kms:%'
+		FROM public.error_view
+		WHERE
+NOT (state='cleared')
 		AND NOT (state='cleared' AND last_checked < CURRENT_DATE - INTERVAL '1 MONTH') $schemaselector
 	", $db1);
 	while ($row=pg_fetch_assoc($result)) {
@@ -53,30 +53,36 @@ if ($f) {
 }
 
 
-$fname = $ERROR_TYPES_FILE .'_'. ((strlen($schema)>0 && $schema!='public') ? $schema : $MAIN_DB_NAME) . '.txt';
+$fname = $ERROR_TYPES_FILE .'_'. (isset($schema) ? $schema : $MAIN_DB_NAME) . '.txt';
 $f = fopen($fname, 'w');
 
 if ($f) {
 
 	// in databases with schemas (like EU and US) there's no public.error_types
 	// table. so pick any schema and take the error_types you'll find there
-	if (!table_exists($db1, 'error_types', $schema)) {
-		$schema=query_firstval("
+	if (!table_exists($db1, 'error_types', $dbschema)) {
+		$dbschema=query_firstval("
 			SELECT schemaname
 			FROM pg_tables
 			WHERE tablename='error_types'
 		", $db1, false);
-		echo "no error_types table found in current schema, using the one in schema $schema instead\n";
+		echo "no error_types table found in current schema, using the one in schema $dbschema instead\n";
 	}
 
-	$result = query("SELECT * FROM $schema.error_types", $db1);
-	while ($row=pg_fetch_assoc($result)) {
-		fwrite($f, $row['error_type'] ."\t". $row['error_name'] ."\t". strtr($row['error_description'], array("\t"=>" ")) ."\n");
-	}
-	pg_free_result($result);
-	fclose($f);
+	if (strlen($dbschema)>0) {
 
-	if ($CREATE_COMPRESSED_DUMPS<>'0') system("bzip2 -c \"$fname\" > \"$fname.bz2\"");
+		$result = query("SELECT * FROM $dbschema.error_types", $db1);
+		while ($row=pg_fetch_assoc($result)) {
+			fwrite($f, $row['error_type'] ."\t". $row['error_name'] ."\t". strtr($row['error_description'], array("\t"=>" ")) ."\n");
+		}
+		pg_free_result($result);
+		fclose($f);
+
+		if ($CREATE_COMPRESSED_DUMPS<>'0') system("bzip2 -c \"$fname\" > \"$fname.bz2\"");
+
+	} else {
+		echo "no error_types table found in any schema. Cannot export error_types.\n";
+	}
 
 } else {
 	echo "Cannot open error-types file ($filename) for writing";
