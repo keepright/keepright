10cc4e6c147a96d35c68be9492287fb5949d1e15
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2009-12-31 15:31:36 +0000

    update rules for poi w/o name, allow some categories to have name OR operator
    git-svn-id: http://svn.code.sf.net/p/keepright/code@269 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0110_point_of_interest_without_name.php
+++ checks/0110_point_of_interest_without_name.php
@@ -3,22 +3,30 @@
 
 /*
 inspired by maplint points of interest are reported if they lack a name (or operator) tag
-*/
 
+NAME required for
+place_of_worship, pub, restaurant, school, university, hospital, library, theatre, courthouse
+
+OPERATOR required for
+bank
+
+NAME or OPERATOR required for
+fast_food, cinema, pharmacy, cafe, fuel
+*/
 
 
 query("
 	CREATE OR REPLACE VIEW _tmp_points_of_interest AS
 	SELECT node_id, MIN(v) as v
 	FROM node_tags
-	WHERE k='amenity' AND v IN ('place_of_worship', 'cinema', 'pharmacy', 'cafe', 'fast_food', 'pub', 'restaurant', 'school', 'university', 'hospital', 'library', 'theatre', 'courthouse')
+	WHERE k='amenity' AND v IN ('place_of_worship', 'pub', 'restaurant', 'school', 'university', 'hospital', 'library', 'theatre', 'courthouse')
 	GROUP BY node_id
 ", $db1);
 query("
 	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
 	SELECT $error_type, 'node', node_id, 'This node is tagged as ' || v || ' and therefore needs a name tag', NOW()
 	FROM _tmp_points_of_interest b
-	WHERE NOT EXISTS( SELECT * FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='name' )
+	WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='name' )
 ", $db1);
 
 
@@ -33,7 +41,22 @@ query("
 	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
 	SELECT $error_type, 'node', node_id, 'This node is tagged as ' || v || ' and therefore needs an operator tag', NOW()
 	FROM _tmp_points_of_interest b
-	WHERE NOT EXISTS( SELECT * FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='operator' )
+	WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='operator' )
+", $db1);
+
+
+query("
+	CREATE OR REPLACE VIEW _tmp_points_of_interest AS
+	SELECT node_id, MIN(v) as v
+	FROM node_tags
+	WHERE k='amenity' AND v IN ('cinema', 'pharmacy', 'cafe', 'fast_food', 'fuel')
+	GROUP BY node_id
+", $db1);
+query("
+	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
+	SELECT $error_type, 'node', node_id, 'This node is tagged as ' || v || ' and therefore needs a name tag or an operator tag', NOW()
+	FROM _tmp_points_of_interest b
+	WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k IN ('name', 'operator'))
 ", $db1);
 
 
