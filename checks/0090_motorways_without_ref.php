497ef479278cbde35fb47b67fcd87a97efe7576a
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2010-02-21 17:59:18 +0000

    include relations in missing-ref-check
    git-svn-id: http://svn.code.sf.net/p/keepright/code@325 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0090_motorways_without_ref.php
+++ checks/0090_motorways_without_ref.php
@@ -4,9 +4,10 @@
 /*
 inspired by maplint motorways are reported if they lack a 'ref', 'nat_ref' or 'int_ref' tag
 to ease tagging, only one of these is required
+
+including relations: It is valid not to have a ref tag if the motorway
+is member of at least one relation that has a ref tag
 */
-
-
 
 query("
 	CREATE OR REPLACE VIEW _tmp_motorways AS
@@ -17,17 +18,23 @@ query("
 ", $db1);
 
 query("
-	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
+	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked)
 	SELECT $error_type, 'way', way_id, 'This way is tagged as motorway and therefore needs a ref, nat_ref or int_ref tag', NOW()
 	FROM _tmp_motorways b
 	WHERE NOT EXISTS (
-		SELECT * FROM way_tags wt
+		SELECT wt.k
+		FROM way_tags wt
 		WHERE wt.way_id=b.way_id AND
 		wt.k IN ('ref', 'nat_ref', 'int_ref')
+
+	) AND NOT EXISTS (
+		SELECT rm.relation_id
+		FROM relation_members rm, relation_tags rt
+		WHERE rm.member_type='W' AND
+		rm.member_id=b.way_id AND rt.relation_id=rm.relation_id AND
+		rt.k IN ('ref', 'nat_ref', 'int_ref')
 	)
 ", $db1);
 
-
 query("DROP VIEW _tmp_motorways", $db1);
-
 ?>
