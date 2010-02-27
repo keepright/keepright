8555858cb19f2c9cc025d59809f6be4309706932
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2010-02-27 16:36:19 +0000

    accept name OR operator for banks
    git-svn-id: http://svn.code.sf.net/p/keepright/code@329 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0110_point_of_interest_without_name.php
+++ checks/0110_point_of_interest_without_name.php
@@ -7,11 +7,8 @@ inspired by maplint points of interest are reported if they lack a name (or oper
 NAME required for
 place_of_worship, pub, restaurant, school, university, hospital, library, theatre, courthouse
 
-OPERATOR required for
-bank
-
 NAME or OPERATOR required for
-fast_food, cinema, pharmacy, cafe, fuel
+bank, fast_food, cinema, pharmacy, cafe, fuel
 */
 
 
@@ -23,7 +20,7 @@ query("
 	GROUP BY node_id
 ", $db1);
 query("
-	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
+	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked)
 	SELECT $error_type, 'node', node_id, 'This node is tagged as ' || v || ' and therefore needs a name tag', NOW()
 	FROM _tmp_points_of_interest b
 	WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='name' )
@@ -34,26 +31,11 @@ query("
 	CREATE OR REPLACE VIEW _tmp_points_of_interest AS
 	SELECT node_id, MIN(v) as v
 	FROM node_tags
-	WHERE k='amenity' AND v IN ('bank')
+	WHERE k='amenity' AND v IN ('bank', 'cinema', 'pharmacy', 'cafe', 'fast_food', 'fuel')
 	GROUP BY node_id
 ", $db1);
 query("
-	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
-	SELECT $error_type, 'node', node_id, 'This node is tagged as ' || v || ' and therefore needs an operator tag', NOW()
-	FROM _tmp_points_of_interest b
-	WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k='operator' )
-", $db1);
-
-
-query("
-	CREATE OR REPLACE VIEW _tmp_points_of_interest AS
-	SELECT node_id, MIN(v) as v
-	FROM node_tags
-	WHERE k='amenity' AND v IN ('cinema', 'pharmacy', 'cafe', 'fast_food', 'fuel')
-	GROUP BY node_id
-", $db1);
-query("
-	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
+	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked)
 	SELECT $error_type, 'node', node_id, 'This node is tagged as ' || v || ' and therefore needs a name tag or an operator tag', NOW()
 	FROM _tmp_points_of_interest b
 	WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k IN ('name', 'operator'))
