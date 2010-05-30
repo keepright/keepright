15e2ed7b82538b8c9a2c8a475e31bf53b816c6ff
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2010-05-30 13:43:48 +0000

    make place of worship wo religion check also care for ways, not just nodes
    git-svn-id: http://svn.code.sf.net/p/keepright/code@365 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0100_places_of_worship_without_religion.php
+++ checks/0100_places_of_worship_without_religion.php
@@ -5,23 +5,22 @@
 inspired by maplint places of worship are reported if they lack a religion tag
 */
 
+$tables = array('node', 'way');
 
+// this loop will execute similar queries for all *_tags tables
+foreach ($tables as $object_type) {
 
-query("
-	CREATE OR REPLACE VIEW _tmp_places_of_worship AS
-	SELECT node_id
-	FROM node_tags
-	WHERE k='amenity' AND v='place_of_worship'
-	GROUP BY node_id
-", $db1);
-
-query("
-	INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, last_checked)
-	SELECT $error_type, 'node', node_id, 'This node is tagged as place of worship and therefore needs a religion tag', NOW()
-	FROM _tmp_places_of_worship b
-	WHERE NOT EXISTS( SELECT * FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k IN ('religion', 'denomination') )
-", $db1);
-
-query("DROP VIEW _tmp_places_of_worship", $db1);
-
+	query("
+		INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, txt1,  last_checked)
+		SELECT $error_type, '{$object_type}', {$object_type}_id, 'This $1 is tagged as place of worship and therefore needs a religion tag', '{$object_type}', NOW()
+		FROM {$object_type}_tags b
+		WHERE k='amenity' AND v='place_of_worship'
+		AND NOT EXISTS(
+			SELECT * FROM {$object_type}_tags t
+			WHERE t.{$object_type}_id=b.{$object_type}_id AND
+			t.k IN ('religion', 'denomination')
+		)
+		GROUP BY {$object_type}_id
+	", $db1);
+}
 ?>
