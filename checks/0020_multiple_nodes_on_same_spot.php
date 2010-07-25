5d33e7d0dcfb855d5f656c395ccde4ff159bd0c7
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2010-07-25 10:21:05 +0000

    remove 1m tolerance from multiple-nodes check
    git-svn-id: http://svn.code.sf.net/p/keepright/code@400 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0020_multiple_nodes_on_same_spot.php
+++ checks/0020_multiple_nodes_on_same_spot.php
@@ -4,38 +4,43 @@
 -- find multiple nodes on the same spot
 -------------------------------------
 
-rounding x/y coordinates leads to a to an intolerance of +/- 0.5 Meters
+in contrast to earlier implementations of this check there is no tolerance
+window any more. Ie only nodes on exactly the same coordinates are considered
+an error. People tend to adding more and more detailed features so precicion
+increased over time and nowadays any minimum allowed distance greater than
+zero may be too large.
 */
 
 
-query("DROP TABLE IF EXISTS _tmp_nodes_rastered", $db1, false);
+
+query("DROP TABLE IF EXISTS _tmp_node_dupes", $db1, false);
 query("
-	CREATE TABLE _tmp_nodes_rastered (
-		x int NOT NULL,
-		y int NOT NULL
+	CREATE TABLE _tmp_node_dupes (
+		lon double precision NOT NULL,
+		lat double precision NOT NULL
 	)
 ", $db1, false);
 
 query("
-	INSERT INTO _tmp_nodes_rastered (x, y)
-	SELECT round(x), round(y)
+	INSERT INTO _tmp_node_dupes (lon, lat)
+	SELECT lon, lat
 	FROM nodes n
-	GROUP BY round(x), round(y)
+	GROUP BY lon, lat
 	HAVING COUNT(id)>1
 ", $db1);
 
+query("CREATE INDEX idx_tmp_node_dupes_xy ON _tmp_node_dupes (lon,lat)", $db1, false);
+query("ANALYZE _tmp_node_dupes", $db1, false);
 
-
-query("CREATE INDEX idx_tmp_nodes_rastered_xy ON _tmp_nodes_rastered (x, y)", $db1, false);
-query("ANALYZE _tmp_nodes_rastered", $db1, false);
 
 query("
 	INSERT INTO _tmp_errors (error_type, object_type, object_id, msgid, txt1, last_checked)
 	SELECT $error_type, 'node', MIN(n.id), 'There is more than one node in this spot. Offending node IDs: $1', group_concat('#' || n.id), NOW()
-	FROM nodes n INNER JOIN _tmp_nodes_rastered r ON (round(n.x)=r.x AND round(n.y)=r.y)
-	GROUP BY r.x, r.y
+	FROM nodes n INNER JOIN _tmp_node_dupes d ON n.lon=d.lon AND n.lat=d.lat
+	GROUP BY d.lon, d.lat
 ", $db1);
 
-query("DROP TABLE IF EXISTS _tmp_nodes_rastered", $db1, false);
+query("DROP TABLE IF EXISTS _tmp_node_dupes", $db1, false);
+
 
 ?>
