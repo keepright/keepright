81f9beb274874f1028782b8825a0a5d75638c88b
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2011-02-22 19:35:11 +0000

    add a valid and distinct name tag to GPX export
    git-svn-id: http://svn.code.sf.net/p/keepright/code@456 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- web/export.php
+++ web/export.php
@@ -117,7 +117,10 @@ if ($_GET['format'] == 'rss') {
 		else
 			$title='';
 
-		echo "\t<wpt lon=\"" . $row['lo'] . "\" lat=\"" . $row['la'] . "\"> <desc><![CDATA[" . $title . $row['error_name'] . ': ' . $row['description'] . "]]></desc><extensions><schema>" . $row['schema'] . "</schema><id>" . $row['error_id'] . "</id></extensions></wpt>\n";
+		echo "\t<wpt lon=\"" . $row['lo'] . "\" lat=\"" . $row['la'] . "\">";
+		echo "<name><![CDATA[" . $row['schema'] . '/' . $row['error_id'] . "]]></name>";
+		echo "<desc><![CDATA[" . $title . $row['error_name'] . ': ' . $row['description'] . "]]></desc>";
+		echo "<extensions><schema>" . $row['schema'] . "</schema><id>" . $row['error_id'] . "</id></extensions></wpt>\n";
 	}
 
 	echo "</gpx>";
