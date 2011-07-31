44634b568f0eba7b9ada74898638f7c6cd682b01
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2011-07-31 15:11:22 +0000

    clean up whitespace
    git-svn-id: http://svn.code.sf.net/p/keepright/code@487 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0140_ways_with_name_without_highway.php
+++ checks/0140_ways_with_name_without_highway.php
@@ -19,10 +19,10 @@ query("
 	INSERT INTO _tmp_errors(error_type, object_type, object_id, description, last_checked) 
 	SELECT $error_type, 'way', way_id, 'This way has a name-tag but no frequently used tag that declares its use (e.g. highway, amenity, building etc)', NOW()
 	FROM _tmp_named_ways b
-	WHERE NOT EXISTS( 
-		SELECT * 
-		FROM way_tags wt 
-		WHERE wt.way_id=b.way_id AND 
+	WHERE NOT EXISTS(
+		SELECT *
+		FROM way_tags wt
+		WHERE wt.way_id=b.way_id AND
 		wt.k NOT IN ('highway', 'history', 'natural', 'railway', 'building', 'amenity', 'boundary', 'leisure', 'aerialway', 'boat', 'bridge', 'tunnel', 'shop', 'area', 'tourism', 'place', 'man_made', 'wood', 'cycleway') )
 ", $db1);
 
