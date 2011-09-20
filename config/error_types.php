4a4d61e9b39a143dfebb28bc2f3153ca08c0e7e6
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2011-09-20 18:05:48 +0000

    reactivate website check
    git-svn-id: http://svn.code.sf.net/p/keepright/code@514 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- config/error_types.php
+++ config/error_types.php
@@ -235,7 +235,7 @@ $error_types[400]['subtype'][403]='adjacent junctions';
 
 
 $error_types[410]['name']='websites';
-$error_types[410]['enabled']=false;
+$error_types[410]['enabled']=true;
 $error_types[410]['source']='0410_website.php';
 $error_types[410]['class']='error';
 $error_types[410]['description']='call websites and try to match website content with key tags from the osm element';
