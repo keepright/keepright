a14929b8d6dc00b95dfdb2bc29c874c1e1dba098
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2011-09-20 10:16:17 +0000

    eliminate shellscripts, switch to binary planet file format
    git-svn-id: http://svn.code.sf.net/p/keepright/code@513 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- planet/pgsql_simple_load.sql
+++ planet/pgsql_simple_load.sql
@@ -1,6 +1,7 @@
 -- add SRID into spatial_ref_sys table
 DELETE FROM spatial_ref_sys WHERE srid = 900913;
-INSERT INTO spatial_ref_sys (srid, auth_name, auth_srid, srtext, proj4text) VALUES 
+
+INSERT INTO spatial_ref_sys (srid, auth_name, auth_srid, srtext, proj4text) VALUES
 (900913,'EPSG',900913,'PROJCS["WGS84 / Simple Mercator",GEOGCS["WGS 84",
 DATUM["WGS_1984",SPHEROID["WGS_1984", 6378137.0, 298.257223563]],PRIMEM["Greenwich", 0.0],
 UNIT["degree", 0.017453292519943295],AXIS["Longitude", EAST],AXIS["Latitude", NORTH]],
@@ -14,11 +15,46 @@ AXIS["y", NORTH],AUTHORITY["EPSG","900913"]]',
 
 
 -- Import the table data from the data files using the fast COPY method.
-\copy nodes FROM 'nodes_sorted.txt' WITH NULL AS 'NULL'
-\copy node_tags FROM 'node_tags.txt' WITH NULL AS 'NULL'
-\copy ways FROM 'ways.txt' WITH NULL AS 'NULL'
-\copy way_tags FROM 'way_tags.txt' WITH NULL AS 'NULL'
-\copy way_nodes FROM 'way_nodes2.txt' WITH NULL AS 'NULL'
-\copy relations FROM 'relations.txt' WITH NULL AS 'NULL'
-\copy relation_tags FROM 'relation_tags.txt' WITH NULL AS 'NULL'
-\copy relation_members FROM 'relation_members.txt' WITH NULL AS 'NULL'
+copy nodes FROM '%TEMPDIR%nodes_sorted.txt' WITH NULL AS 'NULL';
+copy node_tags FROM '%TEMPDIR%node_tags.txt' WITH NULL AS 'NULL';
+copy ways FROM '%TEMPDIR%ways.txt' WITH NULL AS 'NULL';
+copy way_tags FROM '%TEMPDIR%way_tags.txt' WITH NULL AS 'NULL';
+copy way_nodes FROM '%TEMPDIR%way_nodes2.txt' WITH NULL AS 'NULL';
+copy relations FROM '%TEMPDIR%relations.txt' WITH NULL AS 'NULL';
+copy relation_tags FROM '%TEMPDIR%relation_tags.txt' WITH NULL AS 'NULL';
+copy relation_members FROM '%TEMPDIR%relation_members.txt' WITH NULL AS 'NULL';
+
+
+
+-- Add the primary keys and indexes back again (except the way bbox index).
+ALTER TABLE ONLY schema_info ADD CONSTRAINT pk_schema_info PRIMARY KEY (version);
+ALTER TABLE ONLY nodes ADD CONSTRAINT pk_nodes PRIMARY KEY (id);
+ALTER TABLE ONLY ways ADD CONSTRAINT pk_ways PRIMARY KEY (id);
+ALTER TABLE ONLY way_nodes ADD CONSTRAINT pk_way_nodes PRIMARY KEY (way_id, sequence_id);
+ALTER TABLE ONLY relations ADD CONSTRAINT pk_relations PRIMARY KEY (id);
+
+CREATE INDEX idx_node_tags_node_id ON node_tags USING btree (node_id);
+CREATE INDEX idx_nodes_geom ON nodes USING gist (geom);
+
+CREATE INDEX idx_way_tags_way_id ON way_tags USING btree (way_id);
+CREATE INDEX idx_way_nodes_node_id ON way_nodes USING btree (node_id);
+
+CREATE INDEX idx_relation_tags_relation_id ON relation_tags USING btree (relation_id);
+CREATE INDEX idx_relations_member_id ON relation_members USING btree (member_id);
+CREATE INDEX idx_relations_member_role ON relation_members USING btree (member_role);
+CREATE INDEX idx_relations_member_type ON relation_members USING btree (member_type);
+
+
+-- Index for keys and values
+DELETE FROM way_tags WHERE v IS NULL;
+DELETE FROM node_tags WHERE v IS NULL;
+
+CREATE INDEX idx_node_tags_k ON node_tags (k);
+CREATE INDEX idx_node_tags_v ON node_tags (v);
+
+CREATE INDEX idx_way_tags_k ON way_tags (k);
+CREATE INDEX idx_way_tags_v ON way_tags (v);
+
+CREATE INDEX idx_relation_tags_k ON relation_tags (k);
+CREATE INDEX idx_relation_tags_v ON relation_tags (v);
+
