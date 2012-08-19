5a6f4fddd639057fd3c5ab084feec990fb165161
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2012-08-19 14:54:19 +0000

    rewrite check, fix layer default 0, fix bridge/tunnel detection
    git-svn-id: http://svn.code.sf.net/p/keepright/code@610 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- checks/0160_wrong_use_of_level_crossing_tag.php
+++ checks/0160_wrong_use_of_level_crossing_tag.php
@@ -10,87 +10,68 @@ have to be tagged in a special way:
   highway are tagged as bridge or as tunnel
 */
 
-$result=query("
-	SELECT DISTINCT nt.node_id
-	FROM node_tags nt
-	WHERE nt.k='railway' AND nt.v='level_crossing'
+
+// find all railway=level_crossing nodes
+query("DROP TABLE IF EXISTS _tmp_nodes", $db1);
+query("
+	CREATE TABLE _tmp_nodes AS
+	SELECT DISTINCT node_id
+	FROM node_tags
+	WHERE k='railway' AND v='level_crossing'
+", $db1);
+
+query("CREATE INDEX idx_tmp_nodes_node_id ON _tmp_nodes (node_id)", $db1);
+query("ANALYZE _tmp_nodes", $db1);
+
+
+// find all ways passing through these nodes
+query("DROP TABLE IF EXISTS _tmp_ways", $db1);
+query("
+	CREATE TABLE _tmp_ways (
+		way_id bigint NOT NULL,
+		node_id bigint NOT NULL,
+		layer text DEFAULT '0'
+	)
+", $db1);
+query("
+	INSERT INTO _tmp_ways (way_id, node_id)
+	SELECT DISTINCT wn.way_id, wn.node_id
+	FROM way_nodes wn INNER JOIN _tmp_nodes n USING (node_id)
+", $db1);
+
+query("CREATE INDEX idx_tmp_ways_way_id ON _tmp_ways (way_id)", $db1);
+query("CREATE INDEX idx_tmp_ways_node_id ON _tmp_ways (node_id)", $db1);
+query("ANALYZE _tmp_ways", $db1);
+
+find_layer_values('_tmp_ways', 'way_id', 'layer', $db1);
+
+ 
+
+query("
+	INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, last_checked)
+	SELECT $error_type, 'node', node_id, 'There are ways in different layers coming together in this railway crossing', NOW()
+	FROM _tmp_ways
+	GROUP BY node_id
+	HAVING COUNT(DISTINCT layer)>1
 ", $db1);
 
 
-while ($row=pg_fetch_assoc($result)) {
 
-	$way_ids=get_way_ids($row['node_id'], $db2);
-
-	if (!same_layer($way_ids, $db2))
-                query("
-                        INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, last_checked)
-                        VALUES ($error_type, 'node', {$row['node_id']}, 'There are ways in different layers coming together in this railway crossing', NOW())
-                ", $db1, false);
-
-	if (tagged('bridge', $way_ids, $db2) || tagged('tunnel', $way_ids, $db2))
-	        query("
-	                INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, last_checked)
-                        VALUES ($error_type, 'node', {$row['node_id']}, 'There are ways tagged as tunnel or bridge coming together in this railway crossing', NOW())
-                ", $db1, false);
-
-}
-pg_free_result($result);
+query("
+	INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, last_checked)
+	SELECT $error_type, 'node', w.node_id, 'There are ways tagged as tunnel or bridge coming together in this railway crossing', NOW()
+	FROM _tmp_ways w
+	WHERE EXISTS (
+		SELECT 1
+		FROM way_tags wt
+		WHERE wt.way_id=w.way_id AND
+		wt.k IN ('bridge', 'tunnel')
+		AND wt.v NOT IN ('no', 'false', '0')
+	)
+", $db1);
 
 
-
-
-// find all way_ids that are connected to given node
-function get_way_ids($node_id, $db1) {
-	$way_ids=array();
-
-	$result=query("
-        	SELECT DISTINCT way_id
-                FROM way_nodes
-                WHERE node_id=$node_id
-	", $db1, false);
-
-	while ($row=pg_fetch_array($result)) {
-		$way_ids[] = $row[0];
-	}
-	pg_free_result($result);
-        return $way_ids;
-}
-
-
-// get a list of way_ids and lookup the existence of layer-tags
-// return true if each way is tagged with the same layer number
-// missing layer-tags are treated as layer 0
-function same_layer($way_ids, $db) {
-
-        $layer=null;
-
-        foreach ($way_ids as $way_id) {
-                $temp = get_tag('layer', $way_id, $db);
-
-                // remember new layer value if none is known up to now
-                if ($layer == null && $temp != null) $layer=$temp;
-
-                // return false if another value is found
-                if ($layer != $temp) return false;
-
-        }
-        return true;
-}
-
-
-// return true if ANY of given way_ids is tagged with $key
-function tagged($key, $way_ids, $db) {
-        foreach ($way_ids as $way_id) 
-                if (get_tag($key, $way_id, $db)!==null) return true;
-
-        return false;
-}
-
-
-// will query for $key in the tags of given way and return $value 
-// or null if the key is not found
-function get_tag($key, $way_id, $db) {
-        return query_firstval("SELECT v FROM way_tags WHERE way_id=$way_id AND k='$key'", $db, false);
-}
+query("DROP TABLE IF EXISTS _tmp_ways", $db1, false);
+query("DROP TABLE IF EXISTS _tmp_nodes", $db1, false);
 
 ?>
