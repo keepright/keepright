0b60c6a2c71c0d5f45f3ee80dcfe9d7e26727b0a
haraldkleiner <haraldkleiner@4e67cf90-0d29-4caa-83dc-599f2b650fb7>
2012-12-02 19:33:55 +0000

    make JOSM zoom to spot of error, select objects and not zoom to fit objects
    git-svn-id: http://svn.code.sf.net/p/keepright/code@654 4e67cf90-0d29-4caa-83dc-599f2b650fb7

--- web/myText.js
+++ web/myText.js
@@ -241,7 +241,7 @@ parseData: function(ajaxRequest) {
 			data['popupContentHTML'] ='<h5>'+error_name+', '+object_type+' <a href="http://www.openstreetmap.org/browse/'+object_type_EN+'/'+object_id+'" target="_blank">'+object_id+'</a></h5>'+
 			'<p class="p1">'+description+'</p>'+
 
-			'<p class="p2">'+txt4+' <a href="http://localhost:8111/load_and_zoom?left=' + (lon-0.001) + '&right=' + (lon-(-0.001)) + '&top=' + (lat-(-0.001)) + '&bottom=' + (lat-0.001) + '&select=' + object_type_EN + object_id + partner_objects +'" target="hiddenIframe" title="'+txt6+'">'+txt5+'</a> ' +
+			'<p class="p2">'+txt4+' <a href="http://localhost:8111/load_and_zoom?left=' + (lon-0.001) + '&right=' + (lon-(-0.001)) + '&top=' + (lat-(-0.001)) + '&bottom=' + (lat-0.001) + '&select=' + object_type_EN + object_id + partner_objects +'&zoom_mode=download" target="hiddenIframe" title="'+txt6+'">'+txt5+'</a> ' +	
 
 			'<a href="http://www.openstreetmap.org/edit?lat=' + lat + '&lon=' + lon + '&zoom=18" target="_blank">'+txt7+'</a></p>' +
 
