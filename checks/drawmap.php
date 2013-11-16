41eb7f27259599b913d2ae18e497fe0162428436
Jan Michel <j.michel@gsi.de>
2013-11-16 23:35:07 +0100

    updated drawmap script

--- checks/drawmap.php
+++ checks/drawmap.php
@@ -14,7 +14,7 @@ $head = <<<ST
 <?xml version="1.0" encoding="ISO-8859-1" standalone="no" ?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"
   "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
-<svg width="3145" height="2937" xmlns="http://www.w3.org/2000/svg"
+<svg width="2032" height="1575" xmlns="http://www.w3.org/2000/svg"
   xmlns:xlink="http://www.w3.org/1999/xlink">
   <title>World Schema Files</title>
   <style>
@@ -23,11 +23,12 @@ $head = <<<ST
 			fill:none;
 			}
 		text {
+                        font-size:8px;
 			fill:black;
 			text-anchor:middle;
 			}
 	</style>
-<image x="0" y="0" width="3145px" height="2953px"
+<image x="0" y="0" width="2032px" height="1575px"
     xlink:href="map.jpg"/>
 ST;
 
@@ -37,19 +38,19 @@ fwrite($fh,$head);
 foreach ($schemas as $k => $v) {
 	$lr = deg2rad($schemas[$k]['left']);
 	$rr = deg2rad($schemas[$k]['right']);
-	$tr = deg2rad(min(84,$schemas[$k]['top']));
-	$br = deg2rad(max(-84,$schemas[$k]['bottom']));
+	$tr = deg2rad(min(80,$schemas[$k]['top']));
+	$br = deg2rad(max(-80,$schemas[$k]['bottom']));
 
   $tm = .5 * log((1+sin($tr))/(1-sin($tr)));
 	$bm = .5 * log((1+sin($br))/(1-sin($br)));
 	
-	$x = $lr*500+1570;
-	$y = 1474-$tm*500;
-	$w = ($rr-$lr)*500;
-	$h = ($tm-$bm)*500;
+	$x = $lr*323.57+1016;
+	$y = 787.5-$tm*323.57;
+	$w = ($rr-$lr)*323.57;
+	$h = ($tm-$bm)*323.57;
 
 	$tx = $x + $w/2;
-	$ty = $y + $h/2+6;
+	$ty = $y + $h/2+3;
 
 	$str = "<g><rect x=\"$x\" y=\"$y\" width=\"$w\" height=\"$h\" />\n";
 	$str .= "<text x=\"$tx\" y=\"$ty\">$k</text>\n";
