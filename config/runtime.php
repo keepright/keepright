6fae9a9f75f633287bb7942df7205bd6ba1f3595
Jan Michel <jan@mueschelsoft.de>
2013-12-01 22:07:31 +0100

    file to adjust settings during runtime

---
+++ config/runtime.php
@@ -0,0 +1,13 @@
+<?php
+//This file is re-read by makelist.php every time a new thread is started.
+
+//Set to 1 to stop processing
+$stop = 0;
+
+
+//Re-write some other variables:
+//This section should be empty by default!
+
+//$threads=10;
+
+?>
