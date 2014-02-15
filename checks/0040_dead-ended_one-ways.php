1782f02c487c4aad91ab0adac615ae790ce024d0
Jan Michel <jan@mueschelsoft.de>
2014-02-15 11:31:18 +0100

    Excluded parking_entrances from check for dead ended one-ways because this mainly points to unmapped parking garages that are unlikely to ever be mapped

--- checks/0040_dead-ended_one-ways.php
+++ checks/0040_dead-ended_one-ways.php
@@ -37,6 +37,11 @@ query("
 	SELECT $error_type, 'way', o.way_id, 'The first node (id $1) of this one-way is not connected to any other way', o.first_node_id, NOW(), 1e7*o.first_node_lat, 1e7*o.first_node_lon
 	FROM _tmp_one_ways o
 	WHERE o.first_node_id<>o.last_node_id AND
+  NOT EXISTS(
+    SELECT way_id
+    FROM node_tags nt
+    WHERE nt.node_id=o.first_node_id AND nt.k='amenity' AND nt.v IN ('parking_entrance','parking')
+    ) AND
 	NOT EXISTS(
 		SELECT way_id
 		FROM way_nodes wn1
@@ -57,6 +62,11 @@ query("
 	SELECT $error_type+1, 'way', o.way_id, 'The last node (id $1) of this one-way is not connected to any other way', o.last_node_id, NOW(), 1e7*o.last_node_lat, 1e7*o.last_node_lon
 	FROM _tmp_one_ways o
 	WHERE o.first_node_id<>o.last_node_id AND
+  NOT EXISTS(
+    SELECT way_id
+    FROM node_tags nt
+    WHERE nt.node_id=o.last_node_id AND nt.k='amenity' AND nt.v IN ('parking_entrance','parking')
+    ) AND
 	NOT EXISTS(
 		SELECT way_id
 		FROM way_nodes wn2
