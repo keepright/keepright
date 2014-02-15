bd44fc4871511fa7ea3278afb0deaec8e319e626
Jan Michel <jan@mueschelsoft.de>
2014-02-15 18:25:07 +0100

    allow brand instead of operator for amenity:fuel

--- checks/0110_point_of_interest_without_name.php
+++ checks/0110_point_of_interest_without_name.php
@@ -8,7 +8,10 @@ NAME required for
 place_of_worship, pub, restaurant, school, university, hospital, library, theatre, courthouse
 
 NAME or OPERATOR required for
-bank, fast_food, cinema, pharmacy, cafe, fuel
+bank, cinema, pharmacy, cafe
+
+NAME or OPERATOR or BRAND required for
+fast_food, fuel
 */
 
 
@@ -31,7 +34,7 @@ query("
 	CREATE OR REPLACE VIEW _tmp_points_of_interest AS
 	SELECT node_id, MIN(v) as v
 	FROM node_tags
-	WHERE k='amenity' AND v IN ('bank', 'cinema', 'pharmacy', 'cafe', 'fast_food', 'fuel')
+	WHERE k='amenity' AND v IN ('bank', 'cinema', 'pharmacy', 'cafe')
 	GROUP BY node_id
 ", $db1);
 query("
@@ -41,6 +44,20 @@ query("
 	WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k IN ('name', 'operator'))
 ", $db1);
 
+
+query("
+  CREATE OR REPLACE VIEW _tmp_points_of_interest AS
+  SELECT node_id, MIN(v) as v
+  FROM node_tags
+  WHERE k='amenity' AND v IN ('fast_food', 'fuel')
+  GROUP BY node_id
+", $db1);
+query("
+  INSERT INTO _tmp_errors(error_type, object_type, object_id, msgid, txt1, last_checked)
+  SELECT $error_type, 'node', node_id, 'This node is tagged as $1 and therefore needs a name, operator or brand tag', htmlspecialchars(v), NOW()
+  FROM _tmp_points_of_interest b
+  WHERE NOT EXISTS( SELECT nt.node_id FROM node_tags nt WHERE nt.node_id=b.node_id AND nt.k IN ('name', 'operator', 'brand'))
+", $db1);
 
 query("DROP VIEW _tmp_points_of_interest", $db1);
 ?>
